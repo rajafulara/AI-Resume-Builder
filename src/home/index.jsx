27e0285e5252aa1ab3dd425252d99aec5943da94
Raja Fulara <rajafulara.rf@gmail.com>
2025-04-29 23:09:15 +0530

    get started button link fix in homepage

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -2,6 +2,7 @@ import Header from '@/components/custom/Header'
 import { UserButton } from '@clerk/clerk-react'
 import { AtomIcon, Edit, Share2 } from 'lucide-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
@@ -21,10 +22,12 @@ function Home() {
           Build Your Resume <span className='text-primary'>With AI</span> </h1>
         <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Effortlessly Craft a Standout Resume with Our AI-Powered Builder</p>
         <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+        <Link to={'/dashboard'}>
             <a href="/dashboard" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 Get Started
                 <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
+            </Link>
  
         </div>
         <div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
@@ -112,12 +115,13 @@ function Home() {
     </div>
 
     <div className="mt-12 text-center">
+    <Link to={'/dashboard'}>
       <a
-        href="/sign-in"
         className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400"
       >
         Get Started Today
       </a>
+      </Link>
     </div>
     </section>
   </div>
